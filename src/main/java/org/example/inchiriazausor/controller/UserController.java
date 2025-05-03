c254819baca76ba58aa250e3c7934619e58ec424
Victor <victor.buscoveanu@gmail.com>
2025-05-03 12:48:09 +0300

    created env

--- src/main/java/org/example/inchiriazausor/controller/UserController.java
+++ src/main/java/org/example/inchiriazausor/controller/UserController.java
@@ -1,18 +1,15 @@
 package org.example.inchiriazausor.controller;
 
-import java.util.List;
-
 import org.example.inchiriazausor.model.User;
 import org.example.inchiriazausor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -21,10 +18,5 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
-    }
-
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
     }
 } 
