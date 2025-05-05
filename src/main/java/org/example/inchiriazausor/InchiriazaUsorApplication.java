ed8ad03f7c9916cb34fbd0f800f553fdcd26f397
Victor <victor.buscoveanu@gmail.com>
2025-05-05 13:17:36 +0300

    docker-compose.yml updated

--- src/main/java/org/example/inchiriazausor/InchiriazaUsorApplication.java
+++ src/main/java/org/example/inchiriazausor/InchiriazaUsorApplication.java
@@ -3,17 +3,11 @@ package org.example.inchiriazausor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class InchiriazaUsorApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        dotenv.entries().forEach(entry ->
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
-
+       
         SpringApplication.run(InchiriazaUsorApplication.class, args);
     }
 
