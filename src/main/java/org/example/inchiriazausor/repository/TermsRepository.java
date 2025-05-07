69cae5a38b763a12421de959013ea3b0caeba64c
CohenAlbertLouis <louisc4421@gmail.com>
2025-05-07 11:18:05 +0300

    Add files via upload

--- src/main/java/org/example/inchiriazausor/repository/TermsRepository.java
+++ src/main/java/org/example/inchiriazausor/repository/TermsRepository.java
@@ -1,6 +1,6 @@
-package main.java.org.example.inchiriazausor.repository;
+package org.example.inchiriazausor.repository;
 
-import main.java.org.example.inchiriazausor.model.Terms;
+import org.example.inchiriazausor.model.Terms;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TermsRepository extends MongoRepository<Terms, String> {
